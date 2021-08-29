f63ce025be24ea7e3077739388cd8a43d50fca19
Jia Junjian <cnrivkaer@outlook.com>
2021-08-29 16:39:13 +0800

    :sparkles: init Android DI for Dagger2 Example Project

---
+++ Dagger2DIExample/README.md
@@ -0,0 +1,3 @@
+# AndroidDIExample Dagger2DIExample
+
+Android DI Example for Dagger2.
---
+++ Dagger2DIExample/app-example-di-dagger2/src/main/java/top/j3dream/example/di/dagger2/MainActivity.kt
@@ -0,0 +1,11 @@
+package top.j3dream.example.di.dagger2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

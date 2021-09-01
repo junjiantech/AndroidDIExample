d84263d07114783874b0cb16de84b90c63e63e19
JiaJunjian <rivkaer@qq.com>
2021-09-01 16:18:59 +0800

    feat: 添加 Android Jetpack 的 Hilt 示例项目初始化配置。 依赖、Application 配置

--- example-di-hilt-android/app-example-di-hilt/src/main/java/top/j3dream/example/di/hilt/CustomApplication.kt
+++ example-di-hilt-android/app-example-di-hilt/src/main/java/top/j3dream/example/di/hilt/CustomApplication.kt
@@ -1,10 +1,12 @@
 package top.j3dream.example.di.hilt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * - 文件描述: 自定义 Application
  * @author Jia Junjian
  * @since 2021/9/1 15:58
  */
+@HiltAndroidApp
 class CustomApplication : Application()

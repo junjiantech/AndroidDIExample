976b5a20f1f0102c2414025598b41aa6006db793
JiaJunjian <rivkaer@qq.com>
2021-09-02 09:36:01 +0800

    doc: 增加对于Hilt单例部分的理解. 添加部分的文档与注释

--- example-di-hilt-android/app-example-di-hilt/src/main/java/top/j3dream/example/di/hilt/di/module/AppHelperModule.kt
+++ example-di-hilt-android/app-example-di-hilt/src/main/java/top/j3dream/example/di/hilt/di/module/AppHelperModule.kt
@@ -5,9 +5,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import top.j3dream.example.di.hilt.di.testmodel.AppHelper
-import javax.inject.Singleton
 
 /**
+ *
+ * 如果要使用 @Singleton 提供单例. 则只能使用 'SingletonComponent::class' 作用域提供全局单例.
+ *
+ * 当我们去掉 @Singleton 时, 因为我们使用的作用域为 SingletonComponent::class。
+ * 重新运行程序会发现 'AppHelper' 仍然是作为单例提供的.
+ *
  * @author Jia Junjian
  * @since 2021/9/1 17:58
  */
@@ -16,6 +21,6 @@ import javax.inject.Singleton
 class AppHelperModule {
 
     @Provides
-    @Singleton
+    //@Singleton
     fun provideAppHelper(): AppHelper = AppHelper()
 }
--- example-di-hilt-android/app-example-di-hilt/src/main/java/top/j3dream/example/di/hilt/di/module/MainModule.kt
+++ example-di-hilt-android/app-example-di-hilt/src/main/java/top/j3dream/example/di/hilt/di/module/MainModule.kt
@@ -7,6 +7,8 @@ import dagger.hilt.android.components.ActivityComponent
 import top.j3dream.example.di.hilt.di.testmodel.SystemPrinter
 
 /**
+ * 当未进行任何注解的时候, 则当前提供的 SystemPrinter 为 @InstallIn 作用域范围内的局部单例.
+ * 如果需要每次都提供一个新的对象使用. 查看 {@see javax.inject.Provider}
  * @author Jia Junjian
  * @since 2021/9/1 17:20
  */
